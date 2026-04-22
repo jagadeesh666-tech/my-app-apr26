96dc2132d9d3a80785a7d8ea91f138208010ce1c
jagadeesh666-tech <jagadeeshp909@gmail.com>
2026-04-22 14:42:26 +0530

    initial commit

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { AppRoutingModule } from "../app-routing.module";
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css'],
+})
+export class DashboardComponent {
+
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { AppRoutingModule } from "../app-routing.module";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent {
+
+}
