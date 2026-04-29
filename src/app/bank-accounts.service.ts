ab2a628b588697c3e4ac387aa4e54e21e2b90783
jagadeesh666-tech <jagadeeshp909@gmail.com>
2026-04-29 17:05:56 +0530

    TaskC-AboutAPI-Integrations

--- src/app/bank-accounts.service.ts
+++ src/app/bank-accounts.service.ts
@@ -7,9 +7,21 @@ import { Observable } from 'rxjs';
 })
 export class BankAccountsService {
 
-  constructor(private httpClient:HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
-  getBanking():Observable<any>{
-    return this.httpClient.get("https://6128991386a213001729f9df.mockapi.io/test/v1/principals");
+  getBanking(): Observable<any> {
+    return this.httpClient.get("https://6128991386a213001729f9df.mockapi.io/test/v1/principals?limit=10&page=1");
   }
+  getaccountdelete(id: string): Observable<any> {
+    return this.httpClient.delete("https://6128991386a213001729f9df.mockapi.io/test/v1/principals/" + id);
+  }
+  filterAccountData(term: string): Observable<any> {
+    return this.httpClient.get("https://6128991386a213001729f9df.mockapi.io/test/v1/principals?filter=" + term);
+  }
+  sortAccounts(col:string, order:string):Observable<any> {
+    return this.httpClient.get("https://6128991386a213001729f9df.mockapi.io/test/v1/principals?sortBy="+col+"&order="+order);
+  }
+  getpaging(page:number): Observable<any> {
+    return this.httpClient.get("https://6128991386a213001729f9df.mockapi.io/test/v1/principals?limit=10&page="+page);
+}
 }
--- src/app/banking/banking.component.ts
+++ src/app/banking/banking.component.ts
@@ -20,5 +20,59 @@ export class BankingComponent {
       }
     )
   }
+  accountdata(id: string){
+    this.BankAccountsService.getaccountdelete(id).subscribe(
+        (data:any)=>{
+          alert("Account Deleted Succesfully!!");
+          location.reload();
+      },
+      (err:any)=>{
+        alert("Internal Server Error");
+      }
+    )
+  }
+
+  term:string ="";
+
+  filterAccounts(){
+    this.BankAccountsService.filterAccountData(this.term).subscribe(
+          (data:any)=>{
+         this.Banking = data;
+  
+      },
+      (err:any)=>{
+        alert("Internal Server Error");
+      }
+    )
+  }
+
+  col:string="";
+  order:string="";
+  sortsAccount(){
+    this.BankAccountsService.sortAccounts(this.col,this.order).subscribe(
+       (data:any)=>{
+         this.Banking = data;
+  
+      },
+      (err:any)=>{
+        alert("Internal Server Error");
+      }
+
+    )
+  }
+  pagesAccount(page:number){
+    this.BankAccountsService.getpaging(page).subscribe(
+          (data:any)=>{
+         this.Banking = data;
+  
+      },
+      (err:any)=>{
+        alert("Internal Server Error");
+    
+      }
+    )
+
+  }
+
 
 }
