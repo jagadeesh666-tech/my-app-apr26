a3b4940abdde8a3bd8d591689647bce979feea1d
jagadeesh666-tech <jagadeeshp909@gmail.com>
2026-05-13 10:56:08 +0530

    authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,7 @@ import { PipesComponent } from './pipes/pipes.component';
 import { UsersComponent } from './users/users.component';
 import { ProductsComponent } from './products/products.component';
 import { VehiclesComponent } from './vehicles/vehicles.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { BankingComponent } from './banking/banking.component';
 import { FlipkartComponent } from './flipkart/flipkart.component';
 import { MailComponent } from './mail/mail.component';
@@ -37,6 +37,7 @@ import { NavComponent } from './nav/nav.component';
 import { ContactUsModule } from './contact-us/contact-us.module';
 import { PricePipe } from './price.pipe';
 import { CapitalDirective } from './capital.directive';
+import { TokenInterceptor } from './token.interceptor';
 @NgModule({
   declarations: [
     AppComponent,
@@ -80,7 +81,13 @@ import { CapitalDirective } from './capital.directive';
     ReactiveFormsModule,
     ContactUsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
